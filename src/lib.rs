1d73ca708aa2046e5ef76e8dd66afea4d1b7571c
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2021-11-12 19:19:48 +0000

    refactor: move things to optimize module

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@ use std::io;
 mod bytecode;
 mod errors;
 mod ir;
+mod optimize;
 mod parsing;
 
 use crate::bytecode::compile_cfg_to_bytecode;
 pub use crate::bytecode::Bytecode;
 pub use crate::errors::CompilationError;
 use crate::ir::{BasicBlock, BlockLabel, ControlFlowGraph, ThreeAddressInstruction};
+use crate::optimize::optimize;
 pub use crate::parsing::{parse, AbstractSyntaxTree, ConditionalID, Statement};
 
 /// Compile the AST down to bytecode.
@@ -83,36 +85,6 @@ pub fn lower(ast: &AbstractSyntaxTree) -> ControlFlowGraph {
     ControlFlowGraph::new(blocks)
 }
 
-fn optimize(cfg: &ControlFlowGraph) -> ControlFlowGraph {
-    let blocks = cfg
-        .blocks()
-        .iter()
-        .map(|block| BasicBlock::new(block.label(), peephole_optimize(block.instructions())))
-        .collect();
-
-    ControlFlowGraph::new(blocks)
-}
-
-fn peephole_optimize(instructions: &[ThreeAddressInstruction]) -> Vec<ThreeAddressInstruction> {
-    use ThreeAddressInstruction::*;
-
-    let mut new_instructions = vec![NoOp];
-
-    for &instr in instructions {
-        match (new_instructions.last(), instr) {
-            (ChangeVal(x), ChangeVal(y)) => {
-                new_instructions.replace_last(ChangeVal(x.wrapping_add(y)));
-            }
-            (ChangeAddr(x), ChangeAddr(y)) => new_instructions.replace_last(ChangeAddr(x + y)),
-            (_, instr) => new_instructions.push(instr),
-        }
-    }
-
-    new_instructions.retain(|instr| !matches!(instr, NoOp));
-
-    new_instructions
-}
-
 /// Prints Bytecode in a pseudo-assembly format.
 pub fn disassemble(code: &[Bytecode]) {
     for (i, instr) in code.iter().enumerate() {
@@ -141,27 +113,7 @@ pub fn print_cfg(cfg: &ControlFlowGraph) {
     }
 }
 
-// Internal data:
-
-trait LastNonEmptyVector<T> {
-    fn last(&self) -> T;
-
-    fn replace_last(&mut self, x: T);
-}
-
-impl<T> LastNonEmptyVector<T> for Vec<T>
-where
-    T: Copy,
-{
-    fn last(&self) -> T {
-        self[self.len() - 1]
-    }
-
-    fn replace_last(&mut self, x: T) {
-        let n = self.len();
-        self[n - 1] = x;
-    }
-}
+// Internal stuff:
 
 impl fmt::Display for Bytecode {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
