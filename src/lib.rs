9e54cb8b13071589f9cabd9226ee9eec63c82c64
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2021-11-13 21:56:02 +0000

    fix: bad imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+use crate::bytecode::compile_cfg_to_bytecode;
+use crate::parsing::AbstractSyntaxTree;
+
 pub mod bytecode;
 pub mod errors;
 pub mod ir;
 mod optimize;
 pub mod parsing;
 
-use crate::bytecode::{compile_cfg_to_bytecode, Bytecode};
+pub use crate::bytecode::Bytecode;
+pub use crate::errors::CompilationError;
 pub use crate::parsing::parse;
-use crate::parsing::AbstractSyntaxTree;
 
 /// Compile the AST down to bytecode.
 pub fn compile_to_bytecode(ast: &AbstractSyntaxTree) -> Vec<Bytecode> {
