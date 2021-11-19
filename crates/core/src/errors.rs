c8f97d0ed101660bd51a22a229c9fc2a725d4b50
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2021-11-19 23:20:54 +0000

    feat: implement std::error::Error for CompilationError

--- crates/core/src/errors.rs
+++ crates/core/src/errors.rs
@@ -1,4 +1,5 @@
 //! All errors that can be _generated_ by the compiler.
+use std::fmt;
 use std::io;
 
 /// Any error that occurs as a result of compiling the source code.
@@ -7,6 +8,23 @@ pub enum CompilationError {
     IOError(io::Error),
     TooManyCloseBrackets,
     TooFewCloseBrackets,
+}
+
+impl std::error::Error for CompilationError {}
+
+impl fmt::Display for CompilationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use CompilationError::*;
+        let s = match self {
+            IOError(err) => err.to_string(),
+            TooManyCloseBrackets => String::from("too many close brackets"),
+            TooFewCloseBrackets => {
+                String::from("not enough close brackets -- did you forget to close some?")
+            }
+        };
+
+        write!(f, "error: {}", s)
+    }
 }
 
 impl From<io::Error> for CompilationError {
