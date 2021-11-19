5df4337fa0af8dc78796b5cf1bb67dcd740aa4ef
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2021-11-19 22:41:56 +0000

    deps: use stuctopt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,22 @@
 extern crate brainmuck_core;
+extern crate structopt;
 
 use brainmuck_core::{BrainmuckProgram, CompilationError};
-use std::env;
 use std::fs;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 const SIZE_OF_UNIVERSE: usize = 4096;
 
 fn main() -> Result<(), CompilationError> {
-    let args: Vec<_> = env::args().collect();
+    let opt = Opt::from_args();
 
-    if args.len() < 2 {
-        println!("brainmuck: you need to provide a program");
-        return Ok(());
-    }
-
-    let source_text = fs::read(&args[1])?;
+    let source_text = fs::read(&opt.program)?;
     let ast = brainmuck_core::parse(&source_text)?;
 
     let mut universe = [0u8; SIZE_OF_UNIVERSE];
 
-    let program: Box<dyn BrainmuckProgram> = if should_use_jit(&args) {
+    let program: Box<dyn BrainmuckProgram> = if opt.should_use_jit() {
         Box::new(brainmuck_core::compile_to_native_code(&ast))
     } else {
         Box::new(brainmuck_core::compile_to_bytecode(&ast))
@@ -30,6 +27,19 @@ fn main() -> Result<(), CompilationError> {
     Ok(())
 }
 
-fn should_use_jit(args: &[String]) -> bool {
-    !args[1..].iter().find(|&arg| arg == "--no-jit").is_some()
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Disable the JIT, using an interpreter instead (slow!)
+    #[structopt(short = "-J", long = "--no-jit")]
+    no_jit: bool,
+
+    /// filename of the program to run
+    #[structopt(name = "PROGRAM")]
+    program: PathBuf,
+}
+
+impl Opt {
+    fn should_use_jit(&self) -> bool {
+        !self.no_jit
+    }
 }
