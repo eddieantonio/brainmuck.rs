3d1c9f9f12a2ce7196fa3cc09fd7a0206f95c437
Eddie Antonio Santos <eddie.santos@ucdconnect.ie>
2021-11-19 23:03:18 +0000

    refactor: extract method: compile program

--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,30 @@ use structopt::StructOpt;
 
 const SIZE_OF_UNIVERSE: usize = 4096;
 
-/// Run
+/// Run the program
 pub fn run(opt: Opt) -> Result<(), CompilationError> {
-    let source_text = fs::read(&opt.program)?;
-    let ast = brainmuck_core::parse(&source_text)?;
+    let program = compile_program(&opt)?;
 
     let mut universe = [0u8; SIZE_OF_UNIVERSE];
-
-    let program: Box<dyn BrainmuckProgram> = if opt.should_use_jit() {
-        Box::new(brainmuck_core::compile_to_native_code(&ast))
-    } else {
-        Box::new(brainmuck_core::compile_to_bytecode(&ast))
-    };
-
     program.run(&mut universe);
 
     Ok(())
 }
 
+fn compile_program(opt: &Opt) -> Result<Box<dyn BrainmuckProgram>, CompilationError> {
+    let source_text = fs::read(&opt.program)?;
+    let ast = brainmuck_core::parse(&source_text)?;
+
+    if opt.should_use_jit() {
+        Ok(Box::new(brainmuck_core::compile_to_native_code(&ast)))
+    } else {
+        Ok(Box::new(brainmuck_core::compile_to_bytecode(&ast)))
+    }
+}
+
+/// optizing Brainfuck JIT compiler
+///
+/// Runs the Brainfuck program by compiling it to machine code.
 #[derive(Debug, StructOpt)]
 pub struct Opt {
     /// Disable the JIT, using an interpreter instead (slow!)
